89c4a46b3fdf3fc07a0afbf04c2acf7612c3d316
Albina Sakhabutdinova <146albina@gmail.ru>
2018-06-13 10:13:04 +0300

    receiver wip

--- src/main/scala/cs500ir/spark/HelloSpark.scala
+++ src/main/scala/cs500ir/spark/HelloSpark.scala
@@ -2,6 +2,8 @@ package cs500ir.spark
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object HelloSpark {
   def main(args: Array[String]): Unit = {
@@ -11,9 +13,19 @@ object HelloSpark {
 //      .config("spark.some.config.option", "some-value")
 //      .getOrCreate()
 
+    LocalUtils.setStreamingLogLevels()
+    val searchStr = "css"
+
     val sparkConf = new SparkConf().setAppName("infoSearch")
     val sc = new SparkContext(sparkConf)
-
+//    val ssc = new StreamingContext(sparkConf, Seconds(20))
+//    val customReceiverStream: DStream[Commit] = ssc.receiverStream(new CommitsReceiver(searchStr))
+//    customReceiverStream.foreachRDD { rdd =>
+//      val commit = rdd
+//
+//    }
+//    ssc.start()
+//    ssc.awaitTermination()
     // todo create RDD
 
     // todo transformations and actions
